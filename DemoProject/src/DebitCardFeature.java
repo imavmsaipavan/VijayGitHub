f347790c088c6b25ff55822fefceaf7baa8f3b41
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-03-31 20:33:59 +0530

    debit card pin regenrated

--- DemoProject/src/DebitCardFeature.java
+++ DemoProject/src/DebitCardFeature.java
@@ -4,5 +4,8 @@ public class DebitCardFeature {
 	public static void main(String[] args) {
       System.out.println("debit card related lost");
 	}
+	public void debitCardPinRegenrate() {
+		System.out.println("debit card pin regenrated");
+	}
 
 }
