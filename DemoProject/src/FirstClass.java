04007e0b49fffe08a907bbc841f5e99949c5a908
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-03-31 20:07:52 +0530

    first program added

--- DemoProject/src/FirstClass.java
+++ DemoProject/src/FirstClass.java
@@ -5,7 +5,8 @@ public class FirstClass {
             
 	}
 	public void funA() {
-		System.out.println("this is first class"); 
+		System.out.println("this is first class");
+		System.out.println("this is first class");
 	}
 
 }
