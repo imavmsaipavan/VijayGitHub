3cf558bcf9a5ce20ade96d30c3047336274f1754
imavmsaipavan <79898987+imavmsaipavan@users.noreply.github.com>
2021-03-31 20:41:18 +0530

    addition

--- DemoProject/src/PullOperation.java
+++ DemoProject/src/PullOperation.java
@@ -3,6 +3,12 @@ public class PullOperation {
 
 	public static void main(String[] args) {
   System.out.println("here we are performing pull operation");
+ 
+	}
+	public void funA() {
+		int a=10;
+		int b=20;
+        int c=a+b;
 	}
 
 }
